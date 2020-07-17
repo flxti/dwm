6514d245230a9d916b56743efa12c6c5a5e1fdd3
flxti <flxx@protonmail.com>
2020-07-17 21:00:47 +0200

    apply ru_gapps patch to deck mode

--- dwm.c
+++ dwm.c
@@ -654,33 +654,41 @@ createmon(void)
 
 void
 deck(Monitor *m) {
-	unsigned int i, n, h, mw, my;
+	unsigned int i, n, h, mw, my, ns;
 	Client *c;
 
 	for(n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
 	if(n == 0)
 		return;
+	if(n == 1){
+		c = nexttiled(m->clients);
+		resize(c, m->wx, m->wy, m->ww - 2 * c->bw, m->wh - 2 * c->bw, 0);
+		return;
+	}
 
 	if(n > m->nmaster) {
 		mw = m->nmaster
 			? m->ww * (m->rmaster ? 1.0 - m->mfact : m->mfact)
 			: 0;
+    ns = m->nmaster > 0 ? 2 : 1;
 		snprintf(m->ltsymbol, sizeof m->ltsymbol, "[%d]", n - m->nmaster);
 	}
-	else
+	else {
 		mw = m->ww;
-	for(i = my = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
+    ns = 1;
+  }
+	for(i = 0, my = m->gappx, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
 		if(i < m->nmaster) {
-			h = (m->wh - my) / (MIN(n, m->nmaster) - i);
-			//resize(c, m->wx, m->wy + my, mw - (2*c->bw), h - (2*c->bw), False);
-			resize(c, m->rmaster ? m->wx + m->ww - mw : m->wx,
-			       m->wy + my, mw - (2*c->bw), h - (2*c->bw), False);
-			my += HEIGHT(c);
+			h = (m->wh - my) / (MIN(n, m->nmaster) - i) - m->gappx;
+			resize(c, m->rmaster ? m->wx + m->ww - mw + m->gappx : m->wx + m->gappx,
+			       m->wy + my, mw - (2*c->bw) - m->gappx*(5-ns)/2, h - (2*c->bw), False);
+      if (my + HEIGHT(c) + m->gappx < m->wh)
+        my += HEIGHT(c) + m->gappx;
 		}
 		else
-			//resize(c, m->wx + mw, m->wy, m->ww - mw - (2*c->bw), m->wh - (2*c->bw), False);
-			resize(c, m->rmaster ? m->wx : m->wx + mw, m->wy,
-			       m->ww - mw - (2*c->bw), m->wh - (2*c->bw), False);
+			resize(c, m->rmaster ? m->wx + m->gappx/ns : m->wx + mw + m->gappx/ns,
+             m->wy + m->gappx, m->ww - mw - (2*c->bw) - m->gappx*(5-ns)/2, 
+             m->wh - (2*c->bw) - 2*m->gappx, False);
 }
 
 void
