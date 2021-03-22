525845a0dea7ab74dbaddd2d7d9205bcce4c63d9
flxti <flxx@protonmail.com>
2021-03-22 22:45:59 +0100

    chromium

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const int gappx              = 5;        /* gap size between windows */
+static const int gappx              = 0;        /* gap size between windows */
 static const int rmaster            = 1;        /* 1 means master is initially on the right */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -85,25 +85,24 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_white, "-sb", col_cyan, "-sf", col_white, "-bw", "2", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray4, "-nf", col_white, "-sb", col_cyan, "-sf", col_white, NULL };
 static const char *termcmd[]  = { "st", "-e", "tmux", NULL };
 static const char *stcmd[]  = { "st", NULL };
-static const char *ranger_cmd[] = { "st", "-e", "ranger" };
+static const char *ranger_cmd[] = { "st", "-e", "ranger", NULL };
+static const char *chromium_cmd[] = { "chromium", NULL };
 /* brightness */
-static const char *brightness_inc_cmd[] = { "xbacklight", "-inc", "5", NULL};
-static const char *brightness_dec_cmd[] = { "xbacklight", "-dec", "5", NULL };
+static const char *brightness_inc_cmd[] = { "xbacklight", "-inc", "3", NULL};
+static const char *brightness_dec_cmd[] = { "xbacklight", "-dec", "3", NULL };
 static const char *brightness_pkill[] = { "pkill", "-RTMIN+2", "dwmblocks", NULL };
 /* volume */
-static const char *volume_inc_cmd[] =   { "pactl", "set-sink-volume", "1", "+5%", NULL };
-static const char *volume_dec_cmd[] =   { "pactl", "set-sink-volume", "1", "-5%", NULL };
+static const char *volume_inc_cmd[] =   { "pactl", "set-sink-volume", "1", "+3%", NULL };
+static const char *volume_dec_cmd[] =   { "pactl", "set-sink-volume", "1", "-3%", NULL };
 static const char *volume_mute_cmd[] =  { "pactl", "set-sink-mute", "1", "toggle", NULL };
 static const char *volume_pkill[] = { "pkill", "-RTMIN+1", "dwmblocks", NULL };
 /* lock */
 static const char *lock_cmd[] =         { "slock", NULL };
 static const char *lock_suspend_cmd[] = { "slock", "systemctl", "suspend", NULL };
 /* screenshots */
-
-//#bindsym --release Shift+Print exec scrot -s 'screenshot_%Y%m%d_%H%M%S.png' -e 'mkdir -p ~/Pictures/Screenshots && mv $f ~/Pictures/Screenshots && xclip -selection clipboard -t image/png -i ~/Pictures/Screenshots/`ls -1 -t ~/Pictures/Screenshots | head -1`' # Area selection
 static const char *screenshot_cmd[] = { "scrot", "-z", "-o", "/tmp/screenshot_%Y%m%d_%H%M%S.png", NULL };
 static const char *screenshot_focus_cmd[] = { "scrot", "-z", "-o", "-u", "/tmp/screenshot_%Y%m%d_%H%M%S.png", NULL };
 /*keyboard backlight brightnessctl --device='tpacpi::kbd_backlight' set 0 */
@@ -114,8 +113,9 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ Mod4Mask,                     XK_Return, spawn,          {.v = termcmd } },
-	{ Mod4Mask,                     XK_t,      spawn,          {.v = stcmd} },
+	{ MODKEY,                       XK_t,      spawn,          {.v = stcmd} },
 	{ MODKEY,                       XK_r,      spawn,          {.v = ranger_cmd } },
+	{ MODKEY,                       XK_g,      spawn,          {.v = chromium_cmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -130,7 +130,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_x,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_c,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
@@ -161,7 +161,7 @@ static Key keys[] = {
 	{ Mod4Mask|ShiftMask,           XK_k,      spawn,          {.v = keyboard_backlight_off_cmd } },
 	{ MODKEY|ShiftMask,             XK_k,      spawn,          {.v = keyboard_backlight_on_cmd } },
 	{ Mod4Mask|ShiftMask,           XK_s,      spawn,          SHCMD("sleep 1s;scrot -s -o -z /tmp/screenshot_%Y%m%d_%H%M%S.png") },
-	{ Mod4Mask,                     XK_c,      spawn,          SHCMD("sleep 1s;scrot -s -o -z -e 'sxiv -b $f'") },
+	{ Mod4Mask,                     XK_c,      spawn,          SHCMD("sleep 1s;scrot -s -o -z -e 'sxiv -b $f' /tmp/screenshot_%Y%m%d_%H%M%S.png") },
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -5 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +5 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
